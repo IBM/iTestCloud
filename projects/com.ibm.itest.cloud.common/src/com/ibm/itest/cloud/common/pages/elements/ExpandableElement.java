37c044f53b684a2f91010be23dc73bf82379f938
Hongyin Huo <hhuo@ca.ibm.com>
2022-12-15 17:45:50 -0500

    Locate the expansion element (if not relative) with the correct waitForElement
    Signed-off-by: Hongyin Huo <hhuo@ca.ibm.com>

--- projects/com.ibm.itest.cloud.common/src/com/ibm/itest/cloud/common/pages/elements/ExpandableElement.java
+++ projects/com.ibm.itest.cloud.common/src/com/ibm/itest/cloud/common/pages/elements/ExpandableElement.java
@@ -368,7 +368,7 @@ protected BrowserElement getExpansionElement(final By expansionLocator) {
 
 	if(isRelativeLocator(expansionLocator)) return this.element.waitForElement(expansionLocator);
 
-	return waitForElement(expansionLocator);
+	return this.browser.waitForElement(expansionLocator, timeout());
 }
 
 /**
