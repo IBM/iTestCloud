59e01e24f146f8abdc19371d1a32552139109324
Hongyin Huo <hhuo@ca.ibm.com>
2022-12-20 21:53:01 -0500

    Reduce the redundancy in CsvWriter.writeNext
    Signed-off-by: Hongyin Huo <hhuo@ca.ibm.com>

--- projects/com.ibm.itest.cloud.common/src/com/ibm/itest/cloud/common/performance/CsvWriter.java
+++ projects/com.ibm.itest.cloud.common/src/com/ibm/itest/cloud/common/performance/CsvWriter.java
@@ -15,9 +15,11 @@ package com.ibm.itest.cloud.common.performance;
 
 import static com.ibm.itest.cloud.common.scenario.ScenarioUtils.LINE_SEPARATOR;
 import static com.ibm.itest.cloud.common.scenario.ScenarioUtils.QUOTE;
+import static com.ibm.itest.cloud.common.utils.CollectionsUtil.getListFromArray;
 
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Collections;
 
 /**
  * Csv Writer class, for writing csv results to disk
@@ -47,17 +49,7 @@ public CsvWriter (final String filePathName, final char breakCharacter){
  * @param csvArray The array of string to write on the line
  */
 public void writeNext(final String[] csvArray) {
-	try {
-		for (int i = 0; i < csvArray.length; i++) {
-			if (i > 0) this.writer.append(this.breakCharacter);
-			this.writer.append(QUOTE).append(csvArray[i]).append(QUOTE);
-		}
-		this.writer.append(LINE_SEPARATOR);
-		this.writer.flush();
-	} catch (IOException e) {
-		System.out.println("Error occured while writing to csv file.");
-		e.printStackTrace();
-	}
+	writeNext(new ArrayList<>(Collections.unmodifiableList(getListFromArray(csvArray))));
 }
 
 /**
